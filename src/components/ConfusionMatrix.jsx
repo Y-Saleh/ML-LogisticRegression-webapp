30e41aef3e1d79094b09dc8952cd7f05e93ac977
yussuf <yussuf@DESKTOP-HS0LICC>
2025-03-05 20:50:30 +0000

    Update ConfusionMatrix.jsx

--- src/components/ConfusionMatrix.jsx
+++ src/components/ConfusionMatrix.jsx
@@ -8,7 +8,7 @@ const ConfusionMatrix = () => {
   }, []);
 
   return (
-    <div >
+    <div className="absolute top-[90%] w-full md:max-w-[50%] max-w-[600px] h-full flex flex-col text-gray-500 p-6" >
       <h2>Confusion Matrix</h2>
       {imageUrl && <img src={imageUrl} alt="Confusion Matrix" />}
     </div>
